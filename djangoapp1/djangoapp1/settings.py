16696b6045868d19aa33b37b0663b3c36347290f
mil29 <74261419+mil29@users.noreply.github.com>
2021-01-17 14:17:30 +0000

    update

--- djangoapp1/djangoapp1/settings.py
+++ djangoapp1/djangoapp1/settings.py
@@ -138,6 +138,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("SoundsTechFuture@gmail.com")
-EMAIL_HOST_PASSWORD = os.environ.get("sxwmbrtsiapwtojj")
+EMAIL_HOST_USER = "soundstechfuture@gmail.com"
+EMAIL_HOST_PASSWORD = "bubba7341"
 
