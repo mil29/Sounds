9c5ebfe81554653fa30ab052fefdaf6cca8df347
mil29 <74261419+mil29@users.noreply.github.com>
2021-02-01 20:15:24 +0000

    update

--- djangoapp1/users/forms.py
+++ djangoapp1/users/forms.py
@@ -11,6 +11,12 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        help_texts = {
+            'username': None,
+            'email': None,
+            'password1': None,
+            'password2': None,
+        }
 
 
 
