1fd5de63206fe17ee94452c75caf84657f63061f
mil29 <74261419+mil29@users.noreply.github.com>
2021-08-08 18:46:21 +0100

    update

--- djangoapp1/djangoapp1/settings.py
+++ djangoapp1/djangoapp1/settings.py
@@ -164,11 +164,20 @@ MESSAGE_TAGS = {
 
 #S3 BUCKETS CONFIG
 
-AWS_ACCESS_KEY_ID = 'AKIA6N4N7KHXFOOT5FM6'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'sounds-connect-app'
+AWS_DEFAULT_ACL = None
 
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+# s3 static settings
+STATIC_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+STATICFILES_STORAGE = 'djangoapp1.storage_backends.StaticStorage'
+# s3 public media settings
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'djangoapp1.storage_backends.PublicMediaStorage'
