7631de654bd18f9dd86a427329381de721a7c1c5
mil29 <74261419+mil29@users.noreply.github.com>
2021-09-24 16:00:42 +0100

    u

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,6 @@
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
