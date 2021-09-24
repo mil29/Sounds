976536ed4eb2fc5eec62577a07f293a62b8034b5
mil29 <74261419+mil29@users.noreply.github.com>
2021-09-24 16:22:53 +0100

    u

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,6 @@
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-echo yes | python manage.py collectstatic --settings=djangoapp1.settings_mark && 
-./start_server.sh
+echo yes | python manage.py collectstatic
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
