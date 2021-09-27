b4b3e0ee3843c149bf7a21b88b4facfc7a5e9a12
mil29 <74261419+mil29@users.noreply.github.com>
2021-09-27 11:50:32 +0100

    U

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo SECRET_KEY=$SECRET_KEY >> .env
-echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID >> .env
-echo AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY >> .env
-echo EMAIL_HOST_PASSWORD=$EMAIL_HOST_PASSWORD >> .env
-echo EMAIL_HOST_USER=$EMAIL_HOST_USER >> .env
+echo SECRET_KEY=${{ secrets.SECRET_KEY }} >> .env
+echo AWS_ACCESS_KEY_ID=${{ secrets.AWS_ACCESS_KEY_ID }} >> .env
+echo AWS_SECRET_ACCESS_KEY=${{ secrets.AWS_SECRET_ACCESS_KEY }} >> .env
+echo EMAIL_HOST_PASSWORD=${{ secrets.EMAIL_HOST_PASSWORD }} >> .env
+echo EMAIL_HOST_USER=${{ secrets.EMAIL_HOST_USER }} >> .env
 
 
 
