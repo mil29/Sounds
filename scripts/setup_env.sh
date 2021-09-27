ec82d6a60d276df6b81d53528af3de109e48d8a1
mil29 <74261419+mil29@users.noreply.github.com>
2021-09-27 12:13:53 +0100

    u

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+touch .env
 echo SECRET_KEY=${{ secrets.SECRET_KEY }} >> .env
 echo AWS_ACCESS_KEY_ID=${{ secrets.AWS_ACCESS_KEY_ID }} >> .env
 echo AWS_SECRET_ACCESS_KEY=${{ secrets.AWS_SECRET_ACCESS_KEY }} >> .env
