9f30908b4ea606c136f2c8b6f1bd33b47ccf8dd9
mil29 <74261419+mil29@users.noreply.github.com>
2021-09-27 18:33:06 +0100

    u

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /home/ubuntu/djangoapp1/djangoapp1/settings
 touch .env
-echo SECRET_KEY=${{secrets.SECRET_KEY}} > .env
-echo AWS_ACCESS_KEY_ID=${{secrets.AWS_ACCESS_KEY_ID}} > .env
-echo AWS_SECRET_ACCESS_KEY=${{secrets.AWS_SECRET_ACCESS_KEY}} > .env
-echo EMAIL_HOST_PASSWORD=${{secrets.EMAIL_HOST_PASSWORD}} > .env
-echo EMAIL_HOST_USER=${{secrets.EMAIL_HOST_USER}} > .env
+echo SECRET_KEY=${{secrets.SECRET_KEY}} >> .env
+echo AWS_ACCESS_KEY_ID=${{secrets.AWS_ACCESS_KEY_ID}} >> .env
+echo AWS_SECRET_ACCESS_KEY=${{secrets.AWS_SECRET_ACCESS_KEY}} >> .env
+echo EMAIL_HOST_PASSWORD=${{secrets.EMAIL_HOST_PASSWORD}} >> .env
+echo EMAIL_HOST_USER=${{secrets.EMAIL_HOST_USER}} >> .env
 
 
 
