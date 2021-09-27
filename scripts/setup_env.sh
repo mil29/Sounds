a29723cc9ca7c9a5863f8495e46ca4486dbca21e
mil29 <74261419+mil29@users.noreply.github.com>
2021-09-27 19:15:43 +0100

    u

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cat /home/ubuntu/Sounds/djangoapp1/djangoapp1/settings/env.txt >> /home/ubuntu/djangoapp1/djangoapp1/settings/.env
+cat /home/ubuntu/Sounds/djangoapp1/djangoapp1/settings/env.txt >> /home/ubuntu/djangoapp1/djangoapp1/settings/env.txt
+
