d66cac2ac7957e882d7447c158ff4176376a1e2d
mil29 <74261419+mil29@users.noreply.github.com>
2021-10-04 15:54:55 +0100

    updates

--- djangoapp1/djangoapp1/settings/local.py
+++ djangoapp1/djangoapp1/settings/local.py
@@ -160,7 +160,7 @@ LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
